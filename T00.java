4a33c9eef6cb0452f4e72a634cb63074b17187bb
simatupangrafael100-alt <simatupangrafael100@gmail.com>
2025-10-03 09:06:58 +0700

    Pair 08
    Selesai memperbaiki

--- T00.java
+++ T00.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.lang.Math;
 
-public class JavaApplication {
+public class T00 {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
